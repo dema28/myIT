993400beb836f278d1a5b84a5ad71724d9823031
Denis Novicov <dema28ster@gmail.com>
2024-12-21 19:43:23 +0100

    My first lessons for python is IT_Step

---
+++ Python/Lessons_26/main.py
@@ -0,0 +1,31 @@
+# print("Hello World!")
+
+# name = '"John Doe"'
+# age = 30
+# is_student = True
+#
+# if age >= 18:
+#     enter = True
+# else:
+#     enter = False
+#
+# print(enter)
+
+# a = 10
+# b = 20
+# name = "John Doe"
+#
+# print(f"Hello, {name}!")
+# print(a + b)
+# print(f"The sum of {a} and {b} is {a + b}")
+
+# fruits = ["apple", "banana", "cherry"]
+# numbers = [1, 2, 3, 4, 5]
+#
+# print(f"{fruits[0]} = {numbers[1]}")
+
+
+# student = {"name": "Dema", "age": 34, "is_student": True}
+# name = student["name"]
+# age = student["age"]
+# print(name + " is " + str(age) + " years old.")
