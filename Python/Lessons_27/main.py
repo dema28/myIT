e81d4307752f9623eab4b552ceedbc43bfd06eae
Denis Novicov <dema28ster@gmail.com>
2025-01-07 21:03:47 +0100

    My lessons for python is IT_Step

---
+++ Python/Lessons_27/main.py
@@ -0,0 +1,136 @@
+# name = "Alise"
+# age = "25"
+# isStudent = True
+# fruits = ["apple", "banana", "cherry"]
+#
+# # print(f"{name} is {age} years old. Is she a student? {isStudent}")
+# #
+# # print(fruits[1])
+#
+#
+# print(5 + int(age))
+# print(5 + float(age))
+#
+#
+# print("5" + str(10))
+#
+# a = 10
+# b = 20
+# print(a + b)
+# print(a - b)
+# print(a % b)
+# print(a / b)
+# print(a // b)
+# print(a ** b)
+# print(a * b)
+# print(a == b)
+# # print(a!= b)
+# from pprint import pprint
+# from tokenize import String
+#
+# print("hello, Alise !")
+# pprint("hello, Alise !")
+#
+# name = "Alise"
+# hi = "hello"
+# string = " "
+#
+# print(f"{hi}, {name}{string}")
+#
+# letter = name[0]
+# substring = name[1:3]
+# print(letter)
+# print(substring)
+#
+# message = "\nProcess \nfinished \nwith \nexit \ncode \n0"
+# print(message)
+from operator import index
+
+# a = 5
+# b = 10
+# sum = a + b
+# print(f"Сумма {a} плюс {b} ровно {sum}")
+
+# text = "Hello, world"
+# print(text[::-1])
+# upper_case = text.upper()
+# print(upper_case)
+# lower_case = text.lower()
+# print(lower_case)
+#
+# print(text.replace("world", "Python"))
+#
+# print(text.count("l"))
+#
+# print(text.find("world"))
+#
+# print(text.startswith("Hello"))
+# print(text.endswith("world"))
+#
+# print(text.split())
+#
+# print(text.strip())
+#
+# print(text.split("o"))
+#
+# print(text.index("l"))
+#
+# print(text.rindex("l"))
+#
+# print(text.isalnum())
+#
+# print(text.isalpha())
+#
+# print(text.isdigit())
+#
+# print(text.islower())
+#
+# print(text.isupper())
+#
+# print(text.istitle())
+#
+# print(text.swapcase())
+#
+# print(text.title())
+#
+# print(text.zfill(50))
+#
+# a = True
+# b = False
+#
+# print(a and b)
+# print(a or b)
+# print(not a)
+#
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+#
+# print(True or True)
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+
+
+# age = 17
+# res = age >= 18
+#
+#     if res:
+#         print("Вы старше 18 лет", res)
+#     else:
+#         print("Вы младше 18 лет", res)
+
+
+key_in = input("Ведите число: ")
+key_in2 = input("Ведите второе число: ")
+sum_result = (int(key_in) + int(key_in2))
+
+
+print(sum_result)
+
+
+
+
+
