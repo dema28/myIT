bd0cd3b5c5e779b50d76553643b250b1b82e7d5c
Denis Novicov <dema28ster@gmail.com>
2025-01-11 18:03:45 +0100

    lesoons 28

---
+++ Python/Lessons_28/main.py
@@ -0,0 +1,74 @@
+# username = "admin"
+# password = "123"
+#
+# input_username = input("Введите логин: ")
+# input_password = input("Введите пароль: ")
+#
+# isUsernameCorrect = username == input_username
+# isPasswordCorrect = password == input_password
+#
+# if isUsernameCorrect and isPasswordCorrect:
+#     print("Вы успешно вошли в систему!")
+# else:
+#     print("Неправильный логин или пароль!")
+#
+#
+# number = 12.2345694576483
+# rounded_number = round(number, 3)
+# print(rounded_number)
+
+
+# age = 18
+# has_ticket = True
+#
+# if age >= 18 and has_ticket:
+#     print("Вы можете войти в кинотеатр.")
+# else:
+#     print("Вы не можете войти в кинотеатр.")
+
+#
+# age1 = 30
+#
+# if age1 >= 18 and age1 <= 21:
+#     print("1")
+# elif age1 >= 21:
+#     print("2")
+# elif age1 >= 30:
+#     print("3")
+
+
+# num = input("Введите число: ")
+#
+# if num >= 1:
+#     print("число положительное")
+# elif num < 0:
+#     print("число отрицательное")
+# else:
+#     print("равно нулю")
+
+
+# num = 15
+#
+# if num % 2 == 0:
+#     print("число четное")
+# else:
+#     print("число нечетное")
+
+
+# year = int(input("Год: "))
+#
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("год високосный")
+#         else:
+#             print("год не високосный")
+#     else:
+#         print("год високосный")
+
+
+
+
+
+
+
