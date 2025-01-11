6a72237b35adf36d454a4fc5679f77fc34565054
Denis Novicov <dema28ster@gmail.com>
2025-01-11 11:08:17 +0100

    Refactor, HomeWork_27(calculator)

--- Python/HomeWork_27/main1.py
+++ Python/HomeWork_27/main1.py
@@ -2,21 +2,26 @@ from HomeWork_27.calculator import Calculator
 from HomeWork_27.input import Input
 from HomeWork_27.output import Output
 
-class Main1:
-    def run(self):
-        key1, key2 = Input.get_numbers()
-        if key1 is not None and key2 is not None:
-            calculators = Calculator(key1, key2)
-            results = {
-                "sum": calculators.add(),
-                "difference": calculators.subtract(),
-                "product": calculators.multiply(),
-                "quotient": calculators.divide(),
 
-            }
-            Output.display_results(results)
+def run() -> None:
+    key1, key2 = Input.get_numbers()
+    if key1 is not None and key2 is not None:
+        calculators = Calculator(key1, key2)
+        results = {
+            "sum": calculators.add(),
+            "difference": calculators.subtract(),
+            "product": calculators.multiply(),
+            "quotient": calculators.divide(),
+        }
+        Output.display_results(results)
+    else:
+        print("Ошибка ввода!")
+
+
+class Main1:
+    pass
 
 if __name__ == "__main__":
     app = Main1()
-    app.run()
+    run()
 
