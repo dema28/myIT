18e4d6811c41be4613ad0e223551bffde4a208fc
Denis Novicov <dema28ster@gmail.com>
2025-01-18 19:36:24 +0100

    Lessons_30

---
+++ Python/Lessons_30/main.py
@@ -0,0 +1,40 @@
+# con = 0
+# while True:
+#     print(con)
+#     con += 1
+#     if con >=500:
+#         break
+#
+# print("next code")
+
+
+# count = 0
+#
+# while True:
+#     inKey = int(input("Введите число: "))
+#     if inKey == 5:
+#         break
+# print("Your key: ", inKey)
+#
+# number = input("Ведите 10-и значное число: ")
+# if (number.isdigit()) and len(number) == 10:
+#     for num in number:
+#         print(num)
+
+
+# month = input('Введите номер месяца (от 1 до 12): ')
+#
+# if month.isdigit():
+#     month = int(month)
+#     if 1 <= month <= 12:
+#         if month in [12, 1, 2]:
+#             print('Winter')
+#         elif month in [3, 4, 5]:
+#             print('Spring')
+#         elif month in [6, 7, 8]:
+#             print('Summer')
+#         elif month in [9, 10, 11]:
+#             print('Autumn')
+#     else:
+#         print("Пожалуйста, введите число в диапазоне от 1 до 12.")
+
