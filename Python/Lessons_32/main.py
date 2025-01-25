d5385d7748221a4a20faeed11bdd807a29bf6098
Denis Novicov <dema28ster@gmail.com>
2025-01-25 17:59:26 +0100

    Lessons_32

---
+++ Python/Lessons_32/main.py
@@ -0,0 +1,116 @@
+# def greet():
+#     print("Hello, World!")
+#
+# greet()
+
+
+# def greet(name):
+#     print(f"Hello, {name}!")
+#
+# greet("Denis")
+
+#
+# def greet(name):
+#     print(f"Hello, {name}!")
+# names = ["Alice", "Bob", "Grigor", "Silva"]
+#
+# for name in names:
+#     if name != "Alice" and name != "Bob":
+#         greet(name)
+
+# def greet_arg(name = "Guest"):
+#     print(f"Hello, {name}!")
+#
+# greet_arg("Alexander")
+# greet_arg()
+
+#
+# def add(a, b):
+#     return a + b
+#
+# result = add(5, 10)
+# print(result)
+
+
+# x = 10
+# def print_value():
+#     global x
+# x += 5
+# print(x)
+
+# def greet():
+#     return "Hello, Denis!"
+#
+# print(greet())
+#
+#
+#
+# def greet_new(name):
+#     return f"Hello, {name}!"
+#
+# print(greet_new("Denis Nov"))
+#
+#
+#
+#
+# def greet_2(name = "Hello"):
+#     return f"{name}, Denis!"
+#
+# print(greet_2())
+#
+#
+#
+# def greet_3(name = "Hell"):
+#     return f"{name}, Denis!"
+#
+# print(greet_3("Hello"))
+#
+#
+# def greet():
+#     print (f"Hello,!")
+#
+# greet()
+
+
+# count = 0
+# def increment(bos):
+#     global count
+#     count += 1
+#
+#     print(f"Count: {count} - Hello {bos}")
+#
+# increment("Hello")
+# increment("Hell")
+
+
+# def number():
+#     num1 = input("Введите число: ")
+#     num2 = input("Введите второе число: ")
+#     return num1, num2
+#
+# num1, num2 = number()
+# print(f"Сумма чисел: {int(num1) + int(num2)}")
+
+# def test(numer):
+#     num_input = input(f"Введите число {numer} number: ")
+#     return num_input
+#
+# num1 = test(1)
+# num2 = test(2)
+# print(f"Сумма чисел: {int(num1) + int(num2)}")
+
+
+def greet(name, num):
+    print(f"Hello, Denis!{name}. in old {num} 15")
+    name = "Bob"
+
+greet("Alexander", 28)
+
+
+
+
+
+
+
+
+
