1c81bbe73537d265a5bf35374d9a4e098c43dfac
Denis Novicov <dema28ster@gmail.com>
2025-02-01 17:59:48 +0100

    Lessons 33

---
+++ Python/Lessons_34/lessons_34.py
@@ -0,0 +1,59 @@
+# def tooth (dust, kefa):
+#     print("взять шетку")
+#     print("насыпать порошок")
+#     print("Почистить")
+#     if dust:
+#         print("Улыбнутся")
+#     if kefa:
+#         print("нет воды")
+#     print("поставить в стакан")
+#
+#
+# tooth(False, True)
+
+
+# string = ["apple", "banana", "cheery", "date", "orange"]
+
+# string = {7: "apple", "key": "banana", 9: "cheery",10: "date", 11: "orange"}
+#
+# # print(string[1])
+# print(string["key"])
+# string["key"] = "black"
+# print(string["key"])
+
+# string = {7: "apple", 8: "banana", 9: "cheery",10: "date", 11: "orange"}
+#
+# for quantity, fruits in string.items():
+#     print(f"{quantity}: {fruits}")
+# print("================================================")
+# for fruit in string.values():
+#     print(fruit)
+
+
+# with open("exemple.txt", "w") as file:
+#     file.write("Hello, world! \nошибка")
+# print("================================================")
+# with open("exemple.txt", "r") as file:
+#     for line in file:
+#         print(line.strip())
+
+
+# def main(exemple):
+#     # with open("exemple.txt", "w") as file:
+#     #     file.write("Привет, мир! \n")
+#     #     print("================================================")
+#     with open("exemple.txt", "r") as file_read:
+#         # for line in file_read:
+#             print()
+#
+#     return main(exemple)
+# print(main(2))
+
+
+def openfile(filename):
+    with open(filename, "r") as file:
+        content = file.read()
+        return content
+print(openfile("exemple.txt"))
+
+
