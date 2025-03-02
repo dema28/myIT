f60efa4c8f9d166d0299688c9d25ac77f31618b4
Denis Novicov <dema28ster@gmail.com>
2025-03-02 18:15:53 +0100

    HomeWork_39_2

--- Python/HomeWork_39_2/HW_39_2.py
+++ Python/HomeWork_39_2/HW_39_2.py
@@ -1,7 +1,6 @@
 import time
 import unittest
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
